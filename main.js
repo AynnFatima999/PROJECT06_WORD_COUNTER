454e42f477e0d9deb394b769029912fd9d17a70a
45-Assiagnments <aaleali999@gmail.com>
2024-06-08 19:16:34 +0500

    WORD-COUNTER

---
+++ main.js
@@ -0,0 +1,17 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+import chalk from "chalk";
+console.log(chalk.bold.yellowBright("\n \t Welcome to Word Counter App"));
+console.log("=".repeat(50));
+let counter = await inquirer.prompt([{
+        name: "sentence",
+        type: "input",
+        message: "Enter words to count:"
+    }]);
+let words = counter.sentence.trim().split(" ");
+console.log("=".repeat(50));
+console.log(chalk.bold("Sentence Words.."));
+console.log((words));
+console.log("=".repeat(50));
+console.log(chalk.bold(`- word counter: ${chalk.bold.blueBright(words.length)}`));
+console.log("=".repeat(50));
